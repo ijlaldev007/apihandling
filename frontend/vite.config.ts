6278324dd6a17ebc33625a012cdf3bf21f766be4
ijlaldev007 <muhammad13ijlal@gmail.com>
2025-05-05 20:43:32 +0500

    tailwind-shadcn

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,5 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from "path"
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -8,5 +10,10 @@ export default defineConfig({
       '/api' : 'http://localhost:3000',
     }
   },
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
